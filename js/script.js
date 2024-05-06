074bb7261a4490ddfed4c47c5a659f042e3c612a
Prince Caballes <prncaballes@proweaver.net>
2024-05-06 23:55:07 +0800

    portfolio v1.4.8 05-06-24

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
+// Function to toggle dark mode
+function toggleDarkMode() {
+  const isDarkMode = document.documentElement.classList.toggle('bgdark');
+  // Save the current theme preference to local storage
+  localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+}
 
+// Check if there's a theme preference saved in local storage
+const savedTheme = localStorage.getItem('theme');
+console.log('Saved theme:', savedTheme);
+if (savedTheme !== 'dark') {
+  // Apply light mode if the saved theme is not 'dark'
+  document.documentElement.classList.remove('bgdark');
+}
 
 // Dark Mode Toggle
 const dark = document.querySelector('.input');
 dark.onclick = function() {
-    document.documentElement.classList.toggle('bgdark')
+  toggleDarkMode();
 }
 
-
 // Loader
-function hideLoader () {
-    document.getElementById('loader-holder').classList.add('hide-loader')
-  }
-  window.onload = function(){
-    setTimeout(hideLoader, 1800)
-  }
+function hideLoader() {
+  document.getElementById('loader-holder').classList.add('hide-loader');
+  requestAnimationFrame(function() {
+      window.scrollTo(0, 0); 
+  });
+}
+
+window.onload = function() {
+  setTimeout(hideLoader, 1800);
+}
