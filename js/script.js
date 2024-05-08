f3d3b3dfc38b6f3035e5bde754280657a7d16cfa
Prince Caballes <prncaballes@proweaver.net>
2024-05-09 06:41:15 +0800

    Portfolio v1.8.7 05-09-24

--- js/script.js
+++ js/script.js
@@ -1,48 +1,50 @@
 // Dark Mode Toggle
-const dark = document.querySelector('.input');
-dark.onclick = function() {
-  document.documentElement.classList.toggle('bgdark');
-}
+const dark = document.querySelector(".input");
+dark.onclick = function () {
+  document.documentElement.classList.toggle("bgdark");
+};
 
 // Loader
 function hideLoader() {
-  document.getElementById('loader-holder').classList.add('hide-loader');
+  document.getElementById("loader-holder").classList.add("hide-loader");
   // Scroll to top when the page loads
-  requestAnimationFrame(function() {
-      window.scrollTo(0, 0); 
+  requestAnimationFrame(function () {
+    window.scrollTo(0, 0);
   });
 }
 
-window.onload = function() {
+window.onload = function () {
   setTimeout(hideLoader, 1800);
-}
-
+};
 
 // Swapping
 function performSwapping() {
   const windowWidth = window.innerWidth;
-  const socMed = document.querySelector('.sidebar-socials');
-  const secCon = document.querySelector('.contact');
-  const box = document.querySelector('.box');
+  const socMed = document.querySelector(".sidebar-socials");
+  const secCon = document.querySelector(".contact");
+  const box = document.querySelector(".box");
 
   if (windowWidth <= 768) {
-      secCon.insertAdjacentElement('afterend', socMed);
+    secCon.insertAdjacentElement("afterend", socMed);
   } else {
-      box.insertAdjacentElement('beforebegin', socMed);
+    box.insertAdjacentElement("beforebegin", socMed);
   }
 }
 
-document.addEventListener('DOMContentLoaded', performSwapping);
-window.addEventListener('resize', performSwapping);
+document.addEventListener("DOMContentLoaded", performSwapping);
+window.addEventListener("resize", performSwapping);
 
 // Bouncing Letters
-const bounceLetter = document.querySelectorAll('.hero-nm span')
-      
-bounceLetter.forEach(span => {
-  span.addEventListener('mouseover', function () {
-      this.classList.add('bounce')
-      setTimeout(function (){
-          this.classList.remove('bounce')
-      }.bind(this), 1000)
-  }) 
-})
+const bounceLetter = document.querySelectorAll(".hero-nm span");
+
+bounceLetter.forEach((span) => {
+  span.addEventListener("mouseover", function () {
+    this.classList.add("bounce");
+    setTimeout(
+      function () {
+        this.classList.remove("bounce");
+      }.bind(this),
+      1000
+    );
+  });
+});
