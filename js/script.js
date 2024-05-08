21659f46500c806c611ecd92976abb039355f728
Prince Caballes <prncaballes@proweaver.net>
2024-05-09 01:06:49 +0800

    Portfolio v1.8.6 05-09-24

--- js/script.js
+++ js/script.js
@@ -34,3 +34,15 @@ function performSwapping() {
 
 document.addEventListener('DOMContentLoaded', performSwapping);
 window.addEventListener('resize', performSwapping);
+
+// Bouncing Letters
+const bounceLetter = document.querySelectorAll('.hero-nm span')
+      
+bounceLetter.forEach(span => {
+  span.addEventListener('mouseover', function () {
+      this.classList.add('bounce')
+      setTimeout(function (){
+          this.classList.remove('bounce')
+      }.bind(this), 1000)
+  }) 
+})
