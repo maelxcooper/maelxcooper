2b52f18712dc00a88941c2dbd6e15ad9b3d9e4f7
Prince Caballes <prncaballes@proweaver.net>
2024-07-27 05:15:57 +0800

    Overflow update v1.2.0

--- js/animation.js
+++ js/animation.js
@@ -1,128 +1,128 @@
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// let animationsInitialized = false;
+let animationsInitialized = false;
 
 function initializeAnimations() {
-//     // Your GSAP animation code here
-//     gsap.from('.logo h2', {
-//         opacity: 0,
-//         delay: 3,
-//         y: -20
-//     });
-//     gsap.from('.container', {
-//         opacity: 0,
-//         rotation: 400,
-//         delay: 3,
-//         y: -20
-//     });
-//     gsap.from('.box', {
-//         opacity: 0,
-//         delay: 3.5,
-//         x: -20
-//     });
-//     gsap.from('.box2', {
-//         opacity: 0,
-//         delay: 3.5,
-//         x: 20
-//     });
-//     gsap.from('.sidebar-socials', {
-//         opacity: 0,
-//         delay: 3,
-//         y: 20
-//     });
+    // Your GSAP animation code here
+    gsap.from('.logo h2', {
+        opacity: 0,
+        delay: 3,
+        y: -20
+    });
+    gsap.from('.container', {
+        opacity: 0,
+        rotation: 400,
+        delay: 3,
+        y: -20
+    });
+    gsap.from('.box', {
+        opacity: 0,
+        delay: 3.5,
+        x: -20
+    });
+    gsap.from('.box2', {
+        opacity: 0,
+        delay: 3.5,
+        x: 20
+    });
+    gsap.from('.sidebar-socials', {
+        opacity: 0,
+        delay: 3,
+        y: 20
+    });
 
-//     const name_letters = $('.hero-nm');
-//     gsap.from(name_letters.children(), {
-//         opacity: 0,
-//         x: 0,
-//         duration: 1,
-//         delay: 4.5,
-//         stagger: {
-//             amount: 1
-//         }
-//     });
+    const name_letters = $('.hero-nm');
+    gsap.from(name_letters.children(), {
+        opacity: 0,
+        x: 0,
+        duration: 1,
+        delay: 4.5,
+        stagger: {
+            amount: 1
+        }
+    });
 
-//     gsap.from('.hero_body .sml-nm', {
-//         opacity: 0,
-//         delay: 4,
-//         y: 20
-//     });
-//     gsap.from('.hero_body h2', {
-//         opacity: 0,
-//         delay: 4,
-//         y: 20
-//     });
+    gsap.from('.hero_body .sml-nm', {
+        opacity: 0,
+        delay: 4,
+        y: 20
+    });
+    gsap.from('.hero_body h2', {
+        opacity: 0,
+        delay: 4,
+        y: 20
+    });
 
-//     gsap.from('.hero_body p', {
-//         opacity: 0,
-//         delay: 4,
-//         x: 20
-//     });
+    gsap.from('.hero_body p', {
+        opacity: 0,
+        delay: 4,
+        x: 20
+    });
 
-    // gsap.from('.btn_con', {
-    //     opacity: 0,
-    //     delay: 3.5,
-    //     y: 20
-    // });
+    gsap.from('.btn_con', {
+        opacity: 0,
+        delay: 3.5,
+        y: 20
+    });
 
-    // gsap.utils.toArray('li').forEach(li => {
-    //     gsap.fromTo(li, {
-    //         opacity: 0,
-    //         x: 150,
-    //         skewX: 30
-    //     }, {
-    //         opacity: 1,
-    //         x: 0,
-    //         skewX: 0,
-    //         duration: 1,
-    //         delay: 0.5,
-    //         scrollTrigger: li
-    //     });
-    // });
+    gsap.utils.toArray('li').forEach(li => {
+        gsap.fromTo(li, {
+            opacity: 0,
+            x: 150,
+            skewX: 30
+        }, {
+            opacity: 1,
+            x: 0,
+            skewX: 0,
+            duration: 1,
+            delay: 0.5,
+            scrollTrigger: li
+        });
+    });
 
-    // gsap.from(".abt_sec", {
-    //     scrollTrigger: ".skilled-list",
-    //     opacity: 0,
-    //     y: 20,
-    //     duration: 1.5,
-    // });
+    gsap.from(".abt_sec", {
+        scrollTrigger: ".skilled-list",
+        opacity: 0,
+        y: 20,
+        duration: 1.5,
+    });
 
-    // gsap.from('.if_pic', {
-    //     scrollTrigger: '.abt_sec',
-    //     opacity: 0,
-    //     scale: 0.5,
-    //     duration: 1,
-    //     delay: 1
-    // });
+    gsap.from('.if_pic', {
+        scrollTrigger: '.abt_sec',
+        opacity: 0,
+        scale: 0.5,
+        duration: 1,
+        delay: 1
+    });
 
-    // gsap.from(".exp_sec", {
-    //     scrollTrigger: ".prwvr",
-    //     opacity: 0,
-    //     y: 20,
-    //     duration: 1.5,
-    // });
+    gsap.from(".exp_sec", {
+        scrollTrigger: ".prwvr",
+        opacity: 0,
+        y: 20,
+        duration: 1.5,
+    });
 
-    // gsap.from(".work_sec", {
-    //     scrollTrigger: ".project_content",
-    //     opacity: 0,
-    //     y: 20,
-    //     duration: 1.5,
-    // });
+    gsap.from(".work_sec", {
+        scrollTrigger: ".project_content",
+        opacity: 0,
+        y: 20,
+        duration: 1.5,
+    });
 
-    // gsap.from(".contact", {
-    //     scrollTrigger: ".con_title",
-    //     opacity: 0,
-    //     y: 20,
-    //     duration: 2,
-    // });
+    gsap.from(".contact", {
+        scrollTrigger: ".con_title",
+        opacity: 0,
+        y: 20,
+        duration: 2,
+    });
 
-    // gsap.from('.project_img', {
-    //     scrollTrigger: '.wrk-if',
-    //     opacity: 0,
-    //     scale: 0.5,
-    //     duration: 1,
-    //     delay: 1
-    // });
+    gsap.from('.project_img', {
+        scrollTrigger: '.wrk-if',
+        opacity: 0,
+        scale: 0.5,
+        duration: 1,
+        delay: 1
+    });
 }
 
 function handleResize() {
